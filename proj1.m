1b62656f2061b7d017adec5c08e3f85e149c7c3e
Fan Shiyu <Fan Shiyu@LAPTOP-7QD15FBF>
2021-11-17 14:57:35 +0100

    tuning pid

--- proj1.m
+++ proj1.m
@@ -8,8 +8,21 @@ TFs=tf(SS);
 g11=tf(num_g11,den_g11);
 
 g_siso=g11;
-pzmap(g_siso);
-opt=bodeoptions;
-opt.PhaseWrapping='on';
-bode(g_siso,opt); grid on;
+%pzmap(g_siso);
+%opt=bodeoptions;
+%opt.PhaseWrapping='on';
+%bode(g_siso,opt); grid on;
 
+%% PID
+s=tf('s');
+% ku=5.25;
+% tu=0;
+% kp=0.7*ku;
+% ki=2.5/tu;
+% kd=tu/6.7;
+kp=-0.0001;
+% cpid=kp*(1+kd*s+ki/s);
+figure(5)
+% step(G1*cpid/(1+G1*cpid));
+step(g_siso*kp/(1+g_siso*kp));
+%turnedsisoolstepinfo=stepinfo(G1);
